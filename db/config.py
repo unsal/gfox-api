292888b11bf93cf9d9e41924bfa121c96775b3ba
unsal <unsal@unsal.org>
2018-05-27 02:02:38 +0300

    Initial commit

---
+++ db/config.py
@@ -0,0 +1,8 @@
+# Veritabanı bağlantısı için
+class Config:
+        # HOST = "bt.ozyegin.edu.tr:5432"
+        HOST = "localhost:5432"
+        DATABASE = "gfox"
+        USER = "postgres"
+        PASSWORD = "Qaz1wsx2!"
+
